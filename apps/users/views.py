5c2cb91e2f80b8dc05612c22431e47319776a5fa
Andy Venet <andy.venet@gmail.com>
2016-06-12 10:05:27 -0400

    Commented post method in user create api view

--- apps/users/views.py
+++ apps/users/views.py
@@ -10,6 +10,12 @@ class UserCreate(APIView):
     Creates a new user.
     """
     def post(self, request):
+        """
+        Creates an user on the system
+        :param request: The request
+        :return: Returns a 201 status code if the user has been created or 400 code if
+        there was an error on the POST parameters.
+        """
         serializer = UserCreateSerializer(data=request.data)
 
         if serializer.is_valid():
