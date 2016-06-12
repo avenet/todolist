75a3095787f4e3794ca1a298d726ad709dc8f9e9
Andy Venet <andy.venet@gmail.com>
2016-06-12 01:03:30 -0400

    Added missing urls for user create

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from rest_framework.authtoken import views as rest_framework_views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+
+from . import views as user_views
+
 api_patterns = [
+    url(r'^users/$', user_views.UserCreate.as_view()),
     url(r'^users/get-token/$', rest_framework_views.obtain_auth_token),
 ]
 
